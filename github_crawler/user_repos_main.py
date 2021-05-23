36dccbe11105e673855f2ced1c9c5a65b8ec5b2b
민재고 <59470650+minjae705@users.noreply.github.com>
2021-05-23 15:29:19 +0900

    fix: edit crawling url and ignore repo_watchers

--- github_crawler/user_repos_main.py
+++ github_crawler/user_repos_main.py
@@ -10,7 +10,7 @@ def user_repos_parser(url):
     stargazers = repo_stargazers(soup,url)
     forks = repo_forks(soup,url)
     user_repos_results={
-        "watchers": watchers,
+#        "watchers": watchers,
         "stargazers":stargazers,
         "forks" :forks
     }
@@ -20,17 +20,33 @@ def user_repos_parser(url):
 def repo_watchers(soup,url):
     watchers_text  = url+"/watchers"
     watchers = soup.find('a', href=watchers_text)
-    watchers = int(watchers.text)
+    if watchers == None:
+        watchers = 0
+    else:
+        watchers = int(watchers.text)
     return watchers
 
 def repo_stargazers(soup,url):
-    stargazers_text  = url+"/watchers"
+    stargazers_text  = url+"/stargazers"
     stargazers = soup.find('a', href=stargazers_text)
-    stargazers = int(stargazers.text)
+    stargazers = stargazers.text
+    stargazers = stargazers.replace('\n','')    
+    stargazers = stargazers.replace(' ','')
     return stargazers
 
 def repo_forks(soup,url):
-    forks_text  = url+"/watchers"
+    forks_text  = url+"/network/members"
     forks = soup.find('a', href=forks_text)
-    forks = int(forks.text)
+    forks = forks.text
+    forks = forks.replace('\n','')
+    forks = forks.replace(' ','')
     return forks
+
+def repo_info(url):
+    user_repos_results = user_repos_parser(url)
+    return user_repos_results
+
+myrepos = []
+
+for i in myrepos:
+    print(i, repo_info(i))
